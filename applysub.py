280962ea8d6216d39761361a6283b7d0aa71aa9e
kirafanautodec <36808542+kirafanautodec@users.noreply.github.com>
2018-04-30 05:30:10 +0900

    Not second detection of blank and nmtg

--- applysub.py
+++ applysub.py
@@ -18,6 +18,15 @@ parser.add_option('-i', '--input',
 parser.add_option('--gray_threshold',
     action="store", dest="gray_threshold",
     help="gray_threshold of binarization", default=160)
+parser.add_option('--blank_extra_pre',
+    action="store", dest="blank_pre",
+    help="inpaint blank before textarea completely reach its position", default=3)
+parser.add_option('--nmtg_extra_pre',
+    action="store", dest="nmtg_pre",
+    help="inpaint nmtg before textarea completely reach its position", default=1)
+parser.add_option('--blank_extra_sub',
+    action="store", dest="blank_sub",
+    help="inpaint blank after textarea start to disappear", default=0)
 parser.add_option('--typed_speed',
     action="store", dest="typed_speed",
     help="typed effect(text rolling) speed, char per s", default=5)
@@ -33,7 +42,7 @@ if (not options.input):
     print("Missing argument for option 'i'.")
     exit(-1)
 if (not os.path.isfile(options.input)):
-    print("Can not open subtitle file " + option.input)
+    print("Can not open video file " + option.input)
     exit(-1)
     
 # input dir
@@ -42,6 +51,10 @@ print("Work directory: " + filepwd)
     
 frame_cmds = {}
 frame_subindex = {}
+frame_haveblank = {}
+frame_havenmtg = {}
+frame_index_temp = 0
+frame_index_temp1 = 0
 # read timestampfile
 timestampfn = filepwd + '/sub/timestamp.txt'
 with open(timestampfn, mode='r', encoding='utf-8') as fp:
@@ -59,6 +72,28 @@ with open(timestampfn, mode='r', encoding='utf-8') as fp:
         if (action == 'S'):
             subindex = int(cmds[2])
             frame_subindex[frame] = subindex
+        if (action == 'T'):
+            for i in range(frame_index_temp, frame - int(options.blank_pre)):
+                frame_haveblank[i] = False
+            frame_index_temp = frame - int(options.blank_pre)
+            for i in range(frame_index_temp1, frame - int(options.nmtg_pre)):
+                frame_havenmtg[i] = False
+            frame_index_temp1 = frame - int(options.nmtg_pre)
+        if (action == 'X'):
+            for i in range(frame_index_temp, frame + int(options.blank_sub)):
+                frame_haveblank[i] = True
+            frame_index_temp = frame + int(options.blank_sub)
+            for i in range(frame_index_temp1, frame):
+                frame_havenmtg[i] = True
+            frame_index_temp1 = frame
+        if (action == 'O'):
+            for i in range(frame_index_temp, frame + 1):
+                frame_haveblank[i] = False
+            frame_index_temp = frame
+            for i in range(frame_index_temp1, frame + 1):
+                frame_havenmtg[i] = False
+            frame_index_temp1 = frame
+#print (frame_haveblank)
 # read translation the main text
 trans = []
 transfn = filepwd + '/sub/trans.txt'
@@ -83,44 +118,6 @@ with open(nmtgsfn, mode='r', encoding='utf-8') as fp:
 print (trans)
 print (nmtgs)
 
-# COLOR DEFINITION
-BLANK_COLOR_MAX = (250, 256, 256)
-BLANK_COLOR_MIN = (190, 190, 210)
-NMTAG_COLOR_MAX = ( 84, 113, 155)
-NMTAG_COLOR_MIN = ( 34,  63, 125)
-
-def is_blank_p(p):
-    return \
-            (BLANK_COLOR_MAX[0] > p[0] > BLANK_COLOR_MIN[0]) and \
-            (BLANK_COLOR_MAX[1] > p[1] > BLANK_COLOR_MIN[1]) and \
-            (BLANK_COLOR_MAX[2] > p[2] > BLANK_COLOR_MIN[2])
-def is_nmtag_p(p):
-    return \
-            (NMTAG_COLOR_MAX[0] > p[0] > NMTAG_COLOR_MIN[0]) and \
-            (NMTAG_COLOR_MAX[1] > p[1] > NMTAG_COLOR_MIN[1]) and \
-            (NMTAG_COLOR_MAX[2] > p[2] > NMTAG_COLOR_MIN[2])
-            
-def is_blank_func(im0, im1, im2):
-    i0 = np.asarray(im0)
-    i1 = np.asarray(im1)
-    for ln in i0:
-        for p in ln:
-            if not is_blank_p(p):
-                return False
-    for ln in i1:
-        for p in ln:
-            if not is_blank_p(p):
-                return False
-    return True
-
-def is_nmtag_func(im2):
-    i2 = np.asarray(im2)
-    for ln in i2:
-        for p in ln:
-            if not is_nmtag_p(p):
-                return False
-    return True
-    
 video_name = options.input
 video = cv2.VideoCapture(video_name)
 fps = video.get(cv2.CAP_PROP_FPS)
@@ -155,20 +152,6 @@ while(video.isOpened()):
     # ROI of TextArea and NameTag
     img_crop = img_rot[520:740, 80:1180]
 
-    # Judge ROI of TextArea
-    img_judge0 = img_crop[78:80, 130:1000]
-    img_judge1 = img_crop[183:185, 130:1000]
-    # Judge ROI of NameTag
-    img_judge2 = img_crop[50:52, 150:400]
-    # TextArea exist?
-    is_blank = is_blank_func(img_judge0, img_judge1, img_judge2)
-    # NameTag exist?
-    is_nmtag = is_nmtag_func(img_judge2)
-
-    # binarization
-    img_gray = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
-    retval, img_bin = cv2.threshold(img_gray, int(options.gray_threshold), 255, cv2.THRESH_BINARY)
-
     if (frame in frame_cmds):
         cmd = frame_cmds[frame]
         if (cmd == 'C'):
@@ -185,8 +168,14 @@ while(video.isOpened()):
             str_typed_cache = ''
             print(frame, "E")
             print(str_todraw)
+            
+    is_blank = frame_haveblank[frame]
+    is_nmtg = frame_havenmtg[frame]
 
     if (is_blank):
+        # binarization
+        img_gray = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
+        retval, img_bin = cv2.threshold(img_gray, int(options.gray_threshold), 255, cv2.THRESH_BINARY)
         img_mask = cv2.bitwise_not(img_bin)
         img_mask[:88, :] = 0
         img_mask[185:,:] = 0
@@ -202,32 +191,35 @@ while(video.isOpened()):
     else:
         img_inpaint = img_crop
 
-    if (is_blank and len(nmtgs[index_sub])):
+    if (is_nmtg and len(nmtgs[index_sub])):
         img_inpaint[5:55, 5:405] = img_nmtg_blank
-
-    img_pil = Image.fromarray(img_inpaint)
-    draw = ImageDraw.Draw(img_pil)
-    str_typed_render = str_typed_cache[:min(len(str_typed_cache), int(float(options.typed_speed) * (frame - last_typed_start + 1)))]
-    str_splited = (str_todraw + str_typed_render).split('\n')
-    font_w, font_h = draw.textsize("LIPFgYTjyXSq|^#%", font = font_text)
-    for (lineindex, text_line) in enumerate(str_splited):
-        draw_x = 170
-        draw_y0 = 162 if (lineindex > 0) else 105
-        for (spanindex, text_span) in enumerate(text_line.split('$')):
-            color = (60, 66, 111) if (spanindex % 2 == 0) else (150, 106, 255)
-            span_w, span_h = draw.textsize(text_span, font = font_text)
-            draw_y = draw_y0 - font_h / 2
-            draw.text((draw_x, draw_y), text_span, fill = color, font = font_text)
-            draw_x += span_w
         
-    if (is_blank and len(nmtgs[index_sub])):
+    if (is_blank):
+        img_pil = Image.fromarray(img_inpaint)
+        draw = ImageDraw.Draw(img_pil)
+        str_typed_render = str_typed_cache[:min(len(str_typed_cache), int(float(options.typed_speed) * (frame - last_typed_start + 1)))]
+        str_splited = (str_todraw + str_typed_render).split('\n')
+        font_w, font_h = draw.textsize("LIPFgYTjyXSq|^#%", font = font_text)
+        for (lineindex, text_line) in enumerate(str_splited):
+            draw_x = 170
+            draw_y0 = 162 if (lineindex > 0) else 105
+            for (spanindex, text_span) in enumerate(text_line.split('$')):
+                color = (60, 66, 111) if (spanindex % 2 == 0) else (150, 106, 255)
+                span_w, span_h = draw.textsize(text_span, font = font_text)
+                draw_y = draw_y0 - font_h / 2
+                draw.text((draw_x, draw_y), text_span, fill = color, font = font_text)
+                draw_x += span_w
+        
+    if (is_nmtg and len(nmtgs[index_sub])):
         img_draw_nmtg = Image.new("RGB", (400, 50))
         draw_nmtg = ImageDraw.Draw(img_draw_nmtg)
         w_nmtg, h_nmtg = draw.textsize(nmtgs[index_sub], font=font_text)
         draw.text((207 - w_nmtg / 2, 30 - font_h / 2), nmtgs[index_sub], fill = (255,255,255), font = font_text)
 
-    img_drawed = np.array(img_pil)
-    img_rot[520:740, 80:1180] = img_drawed
+    if (is_blank):
+        img_drawed = np.array(img_pil)
+        img_rot[520:740, 80:1180] = img_drawed
+
     cv2.imshow("img_merged", img_rot)
     cv2.waitKey(1)
     out_video.write(img_rot)
@@ -235,6 +227,7 @@ while(video.isOpened()):
 video.release()
 out_video.release()
 
+exit(0)
 ffcmd = "ffmpeg -y -vn -i " + video_name + " -acodec copy " + video_name + ".aac"
 print (ffcmd)
 subprocess.call(ffcmd, shell=True)
