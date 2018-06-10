6f23a014c87912fc17abb14f434b753502cefaec
Ayaya_Komichi <kirafan_autodec@yahoo.co.jp>
2018-06-10 23:15:01 +0900

    Search Nmtg Transition and patch it, allow user to input 3 lines

--- analyse_video.py
+++ analyse_video.py
@@ -2,7 +2,8 @@
 import numpy as np
 import cv2
 import subprocess
-import os, sys
+import os
+import sys
 import optparse
 
 try:
@@ -11,26 +12,37 @@ except:
     pass
 
 python_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-currPath = sys.path[0]  
+currPath = sys.path[0]
 highPath = os.path.split(currPath)[0]
 env = os.environ.copy()
 spliter = ';' if os.name == 'nt' else ':'
 env["PATH"] = python_dir + spliter + highPath + spliter + env["PATH"]
 
+pattern0_f = python_dir + "/usr/pattern0.png"
+if (not os.path.isfile(pattern0_f)):
+    pattern0_f = highPath + "/usr/pattern0.png"
+if (not os.path.isfile(pattern0_f)):
+    print("Can not load nmtg pattern0" + pattern0_f)
+img_pattern0 = cv2.imread(pattern0_f)
+
 # Parse options
 parser = optparse.OptionParser()
 parser.add_option('-i', '--input',
-    action="store", dest="input",
-    help="input file of video file")
+                  action="store", dest="input",
+                  help="input file of video file")
 parser.add_option('--gray_threshold',
-    action="store", dest="gray_threshold",
-    help="gray_threshold of binarization", default=160)
+                  action="store", dest="gray_threshold",
+                  help="gray_threshold of binarization", default=160)
 parser.add_option('--textpos_threshold',
-    action="store", dest="textpos_threshold",
-    help="text position detection threshold", default=5)
+                  action="store", dest="textpos_threshold",
+                  help="text position detection threshold", default=5)
 parser.add_option('--wait_frame_threshold',
-    action="store", dest="wait_frame_threshold",
-    help="text pause detection threshold in frame", default=2)
+                  action="store", dest="wait_frame_threshold",
+                  help="text pause detection threshold in frame", default=2)
+parser.add_option('--nmtg_tm_threshold',
+                  action="store", dest="nmtg_tm_threshold",
+                  help="nmtg box template matching threshold", default=0.12)
+
 
 options, args = parser.parse_args()
 if (not len(args)):
@@ -40,21 +52,22 @@ inputvideo = os.path.abspath(args[0])
 if (not os.path.isfile(inputvideo)):
     print("Can not open video file " + inputvideo)
     exit(-1)
-    
+
 # output dir
 print("Inputvideo: " + inputvideo)
 basename = os.path.basename(inputvideo)
 dirname = os.path.dirname(inputvideo)
 output_dir = dirname + ('/' if dirname else '') + 'autosub'
 
-# re encode 
+# re encode
 reencode_video_name = inputvideo + '_reencode.mp4'
 print("Re encode to CFR Video file: " + inputvideo)
-ffcmd = "ffmpeg -hide_banner -y -i " + inputvideo + " -c:v mpeg4 -b:v 24000k -r 30 -s 1280x720 -acodec aac -strict -2 -ac 2 -ab 256k -ar 44100 -f mp4 " + reencode_video_name
+ffcmd = "ffmpeg -hide_banner -y -i " + inputvideo + \
+    " -c:v mpeg4 -b:v 24000k -r 30 -s 1280x720 -acodec aac -strict -2 -ac 2 -ab 256k -ar 44100 -f mp4 " + reencode_video_name
 " aac -strict -2 -ac 2 -ab 256k -ar 44100 -f "
-print ("Invoking: " + ffcmd)
+print("Invoking: " + ffcmd)
 subprocess.call(ffcmd, shell=True, env=env)
-print ("Re-encoding Finished")
+print("Re-encoding Finished")
 
 script_output = output_dir + '/' + basename + '.krfss'
 img_output_dir = output_dir + '/' + basename + '_img'
@@ -76,12 +89,15 @@ BLANK_COLOR_MAX = (250, 256, 256)
 BLANK_COLOR_MIN = (190, 190, 210)
 
 # blank color of textarea?
+
+
 def is_blank_p(p):
     return \
-            (BLANK_COLOR_MAX[0] > p[0] > BLANK_COLOR_MIN[0]) and \
-            (BLANK_COLOR_MAX[1] > p[1] > BLANK_COLOR_MIN[1]) and \
-            (BLANK_COLOR_MAX[2] > p[2] > BLANK_COLOR_MIN[2])
-            
+        (BLANK_COLOR_MAX[0] > p[0] > BLANK_COLOR_MIN[0]) and \
+        (BLANK_COLOR_MAX[1] > p[1] > BLANK_COLOR_MIN[1]) and \
+        (BLANK_COLOR_MAX[2] > p[2] > BLANK_COLOR_MIN[2])
+
+
 def is_blank_func(im0, im1):
     i0 = np.asarray(im0)
     i1 = np.asarray(im1)
@@ -96,10 +112,12 @@ def is_blank_func(im0, im1):
     return True
 
 # judge if two nmtg_img are same
+
+
 def is_same_name(img0, img1):
-    kernel = np.ones((3,3), np.uint8)
-    img0_dil_inv = cv2.bitwise_not(cv2.dilate(img0, kernel, iterations = 1))
-    img1_dil_inv = cv2.bitwise_not(cv2.dilate(img1, kernel, iterations = 1))
+    kernel = np.ones((3, 3), np.uint8)
+    img0_dil_inv = cv2.bitwise_not(cv2.dilate(img0, kernel, iterations=1))
+    img1_dil_inv = cv2.bitwise_not(cv2.dilate(img1, kernel, iterations=1))
     img_judge0 = cv2.bitwise_and(img0, img1_dil_inv)
     img_judge1 = cv2.bitwise_and(img1, img0_dil_inv)
     return 0 == np.sum(img_judge0) + np.sum(img_judge1)
@@ -107,27 +125,33 @@ def is_same_name(img0, img1):
 # get_index in nmtg db
 # if no same name in the db
 # then append
+
+
 def get_nmtg_index(img1):
     for (i, img0) in enumerate(g_nmtg_img_db):
         if is_same_name(img0, img1):
             return i
     g_nmtg_img_db.append(img1.copy())
     i = len(g_nmtg_img_db) - 1
-    cv2.imwrite(img_output_dir + '/' + "/nmtg_" + ("%04d"%i)+".png", img1)
+    cv2.imwrite(img_output_dir + '/' + "/nmtg_" + ("%04d" % i)+".png", img1)
     return i
+
 
 # POSITION DEFINITION
 ROI = (slice(500, 710), slice(70, 1140))
 ROI_JUDGE0 = (slice(70, 72), slice(130, 1000))
 ROI_JUDGE1 = (slice(178, 180), slice(130, 1000))
 ROI_NMTG = (slice(7, 50), slice(15, 395))
+ROI_NMTG_SEARCH = (slice(0, 210), slice(13, 13 + 382))
 LINE_START = 169
 LINE_LENGTH = 800
 LINE_HEIGHT = 36
 ROI_LINE0_Y = 84
 ROI_LINE1_Y = 136
-ROI_LINE0 = (slice(ROI_LINE0_Y, ROI_LINE0_Y + LINE_HEIGHT), slice(LINE_START, LINE_START + LINE_LENGTH))
-ROI_LINE1 = (slice(ROI_LINE1_Y, ROI_LINE1_Y + LINE_HEIGHT), slice(LINE_START, LINE_START + LINE_LENGTH))
+ROI_LINE0 = (slice(ROI_LINE0_Y, ROI_LINE0_Y + LINE_HEIGHT),
+             slice(LINE_START, LINE_START + LINE_LENGTH))
+ROI_LINE1 = (slice(ROI_LINE1_Y, ROI_LINE1_Y + LINE_HEIGHT),
+             slice(LINE_START, LINE_START + LINE_LENGTH))
 
 # input video
 video_name = reencode_video_name
@@ -142,7 +166,7 @@ last_textpos_store = 0
 
 # Status
 # 0: No text in textarea, waiting for text
-# 1: 
+# 1:
 status = 0
 
 # Storing
@@ -179,7 +203,8 @@ while(video.isOpened()):
 
     # binarization
     img_gray = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
-    retval, img_bin = cv2.threshold(img_gray, int(options.gray_threshold), 255, cv2.THRESH_BINARY)
+    retval, img_bin = cv2.threshold(img_gray, int(
+        options.gray_threshold), 255, cv2.THRESH_BINARY)
 
     # Invert NameTag Area
     img_nmtg = cv2.bitwise_not(img_bin[ROI_NMTG])
@@ -189,48 +214,64 @@ while(video.isOpened()):
     img_line1 = img_bin[ROI_LINE1]
     # Concat 2 lines horizontally
     img_line = np.concatenate((img_line0, img_line1), axis=1)
-    
+
     img_line0_color = img_crop[ROI_LINE0]
     img_line1_color = img_crop[ROI_LINE1]
-    img_line0_color = cv2.bitwise_or(img_line0_color, cv2.cvtColor(img_line0, cv2.COLOR_GRAY2BGR))
-    img_line1_color = cv2.bitwise_or(img_line1_color, cv2.cvtColor(img_line1, cv2.COLOR_GRAY2BGR))
+    img_line0_color = cv2.bitwise_or(
+        img_line0_color, cv2.cvtColor(img_line0, cv2.COLOR_GRAY2BGR))
+    img_line1_color = cv2.bitwise_or(
+        img_line1_color, cv2.cvtColor(img_line1, cv2.COLOR_GRAY2BGR))
     if (frame == 1):
-        img_line_color = np.concatenate((img_line0_color, img_line1_color), axis=0)
+        img_line_color = np.concatenate(
+            (img_line0_color, img_line1_color), axis=0)
+
+    # Template Matching for NMTG
+    (h, w, c) = img_pattern0.shape
+    img_tm = cv2.matchTemplate(
+        img_crop[ROI_NMTG_SEARCH], img_pattern0, cv2.TM_SQDIFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(img_tm)
+    if (min_val < float(options.nmtg_tm_threshold)):
+        nmtg_y = min_loc[1]
+        if (min_loc[1] > 1):
+            print("Nmtg Transition", frame, nmtg_y)
+            timestamp_data.append(
+                {"at": frame, "action": "N", "y": nmtg_y}
+            )
 
     # If TextArea does not exits
     # clear the textarealine
     if (not is_blank):
         img_line[:] = 255
 
-    # Rotated textarea line 90 deg  
+    # Rotated textarea line 90 deg
     arr_line_rot = np.array(np.rot90(img_line))
-    # Scan the 'x position' of last character => textpos        
+    # Scan the 'x position' of last character => textpos
     textpos = arr_line_rot.shape[0]
     for col in arr_line_rot:
         textpos -= 1
         if (not np.all(col)):
             break
 
-    # detect change of textpos   
+    # detect change of textpos
     is_textpos_changed = 0
     # avoid jittering of textpos
     if (abs(textpos - last_textpos) > int(options.textpos_threshold)):
-        is_textpos_changed = 1 if (textpos - last_textpos) > 0 else -1 
+        is_textpos_changed = 1 if (textpos - last_textpos) > 0 else -1
 
     if (not is_blank_last == int(is_blank)):
         if (is_blank):
-            print ("TextArea In", frame)
+            print("TextArea In", frame)
             timestamp_data.append({"at": frame, "action": "T"})
         else:
-            print ("TextArea Out", frame)
+            print("TextArea Out", frame)
             timestamp_data.append({"at": frame, "action": "X"})
         is_blank_last = int(is_blank)
-    
+
     # NO TEXT, waiting for a new line
     if (status == 0):
         # Start of a Line
         if (textpos > 0):
-            print ("LN", frame)
+            print("LN", frame)
             timestamp_data.append({"at": frame, "action": "L"})
             # Status -> 1
             status = 1
@@ -239,46 +280,54 @@ while(video.isOpened()):
             last_textpos_store = 0
             if (storing == 0):
                 print("Start", frame)
-                timestamp_data.append({"at": frame, "action": "S", "sub": index_sub})
+                timestamp_data.append(
+                    {"at": frame, "action": "S", "sub": index_sub})
                 storing = 1
 
     # Monitoring change of textarea
     elif (status == 1):
-        # new text + 
+        # new text +
         if (is_textpos_changed > 0):
             last_change = frame
             if (storing == 0):
                 print("Start", frame)
-                timestamp_data.append({"at": frame, "action": "S", "sub": index_sub})
+                timestamp_data.append(
+                    {"at": frame, "action": "S", "sub": index_sub})
                 storing = 1
         # Keep
         if (is_textpos_changed == 0):
             # wait <wait_frame_threshold> and then store.
             if (frame - last_change > int(options.wait_frame_threshold) and abs(last_textpos_store - textpos) > int(options.textpos_threshold)):
                 frame_real = frame - int(options.wait_frame_threshold)
-                print ("End", frame_real)
-                timestamp_data.append({"at": frame_real, "action": "E", "sub": index_sub})
+                print("End", frame_real)
+                timestamp_data.append(
+                    {"at": frame_real, "action": "E", "sub": index_sub})
                 storing = 0
                 # Store TextArea
                 img_line0_color_c = img_line0_color.copy()
                 img_line1_color_c = img_line1_color.copy()
-                cv2.line(img_line0_color_c, \
-                         (last_textpos_store + 10, 30), (textpos, 30), \
+                cv2.line(img_line0_color_c,
+                         (last_textpos_store + 10, 30), (textpos, 30),
                          (127, 255, 0), 20)
-                cv2.line(img_line1_color_c, \
-                         (last_textpos_store + 10 - LINE_LENGTH, 30), (textpos - LINE_LENGTH, 30), \
+                cv2.line(img_line1_color_c,
+                         (last_textpos_store + 10 - LINE_LENGTH,
+                          30), (textpos - LINE_LENGTH, 30),
                          (127, 255, 0), 20)
-                img_line_color_o = np.concatenate((img_line0_color, img_line1_color), axis=0)
-                img_line_color_c = np.concatenate((img_line0_color_c, img_line1_color_c), axis=0)
-                img_line_color = cv2.addWeighted(img_line_color_o, 0.85, img_line_color_c, 0.15, 1)
-                cv2.imwrite(img_output_dir + '/' + "text_" + ("%04d"%index_sub)+".png", img_line_color)
-                print ("index_sub: " + str(index_sub))
-                
+                img_line_color_o = np.concatenate(
+                    (img_line0_color, img_line1_color), axis=0)
+                img_line_color_c = np.concatenate(
+                    (img_line0_color_c, img_line1_color_c), axis=0)
+                img_line_color = cv2.addWeighted(
+                    img_line_color_o, 0.85, img_line_color_c, 0.15, 1)
+                cv2.imwrite(img_output_dir + '/' + "text_" +
+                            ("%04d" % index_sub)+".png", img_line_color)
+                print("index_sub: " + str(index_sub))
+
                 # Store NameTag
                 nmtg_index = get_nmtg_index(img_nmtg)
                 nmtg_map.append(nmtg_index)
-                print ("nmtg_index: " + str(nmtg_index))
-                
+                print("nmtg_index: " + str(nmtg_index))
+
                 index_sub += 1
                 last_textpos_store = textpos
 
@@ -286,14 +335,14 @@ while(video.isOpened()):
         if (is_textpos_changed < 0):
             print("CR", frame)
             timestamp_data.append({"at": frame, "action": "C"})
-            status = 0    
+            status = 0
 
     cv2.imshow("img_crop", img_crop)
     cv2.imshow("img_sub", img_line_color)
     cv2.waitKey(1)
     last_textpos = textpos
 
-print ("All End", frame)
+print("All End", frame)
 video.release()
 timestamp_data.append({"at": frame, "action": "O"})
 
--- patch_subtitle.py
+++ patch_subtitle.py
@@ -4,7 +4,8 @@ import numpy as np
 import cv2
 import codecs
 import subprocess
-import os, sys
+import os
+import sys
 import optparse
 
 try:
@@ -13,7 +14,7 @@ except:
     pass
 
 python_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-currPath = sys.path[0]  
+currPath = sys.path[0]
 highPath = os.path.split(currPath)[0]
 env = os.environ.copy()
 spliter = ';' if os.name == 'nt' else ':'
@@ -33,41 +34,41 @@ if (not os.path.isfile(nmtg_blank_f)):
 
 parser = optparse.OptionParser()
 parser.add_option('-i', '--input',
-    action="store", dest="input",
-    help="input file of video file")
+                  action="store", dest="input",
+                  help="input file of video file")
 parser.add_option('--gray_threshold',
-    action="store", dest="gray_threshold",
-    help="gray_threshold of binarization", default=160)
+                  action="store", dest="gray_threshold",
+                  help="gray_threshold of binarization", default=160)
 parser.add_option('--blank_extra_pre',
-    action="store", dest="blank_pre",
-    help="inpaint blank before textarea completely reach its position", default=3)
+                  action="store", dest="blank_pre",
+                  help="inpaint blank before textarea completely reach its position", default=3)
 parser.add_option('--nmtg_extra_pre',
-    action="store", dest="nmtg_pre",
-    help="inpaint nmtg before textarea completely reach its position", default=0)
+                  action="store", dest="nmtg_pre",
+                  help="inpaint nmtg before textarea completely reach its position", default=0)
 parser.add_option('--blank_extra_sub',
-    action="store", dest="blank_sub",
-    help="inpaint blank after textarea start to disappear", default=0)
+                  action="store", dest="blank_sub",
+                  help="inpaint blank after textarea start to disappear", default=0)
 parser.add_option('--typed_speed',
-    action="store", dest="typed_speed",
-    help="typed effect(text rolling) speed, char per s", default=5)
+                  action="store", dest="typed_speed",
+                  help="typed effect(text rolling) speed, char per s", default=5)
 parser.add_option('--fontsize',
-    action="store", dest="fontsize",
-    help="font size of translated text", default=35)
+                  action="store", dest="fontsize",
+                  help="font size of translated text", default=35)
 parser.add_option('--fontsize_nmtg',
-    action="store", dest="fontsize_nmtg",
-    help="font size of translated name tag", default=33)
+                  action="store", dest="fontsize_nmtg",
+                  help="font size of translated name tag", default=33)
 parser.add_option('--ffmpeg_encoder',
-    action="store", dest="ffmpeg_encoder",
-    help="ffmpeg encoder, default = libx264, use h264_nvenc if available", default="libx264")
+                  action="store", dest="ffmpeg_encoder",
+                  help="ffmpeg encoder, default = libx264, use h264_nvenc if available", default="libx264")
 parser.add_option('--bitrate',
-    action="store", dest="bitrate",
-    help="ffmpeg vcodec -b:v", default="1780k")
+                  action="store", dest="bitrate",
+                  help="ffmpeg vcodec -b:v", default="6000k")
 parser.add_option('--audio_bitrate',
-    action="store", dest="audio_bitrate",
-    help="ffmpeg acodec -ar", default="192k")
+                  action="store", dest="audio_bitrate",
+                  help="ffmpeg acodec -ar", default="192k")
 parser.add_option('--overlap',
-    action="store_true", dest="overlap",
-    help="overlap text on japanese, for debugging")
+                  action="store_true", dest="overlap",
+                  help="overlap text on japanese, for debugging")
 
 options, args = parser.parse_args()
 if (not len(args)):
@@ -77,7 +78,7 @@ inputvideo = os.path.abspath(args[0])
 if (not os.path.isfile(inputvideo)):
     print("Can not open video file " + inputvideo)
     exit(-1)
-    
+
 # output dir
 if (inputvideo[-13:] == '_reencode.mp4'):
     inputvideo = inputvideo[:-13]
@@ -87,7 +88,7 @@ reencode_video_name = inputvideo + '_reencode.mp4'
 if (not os.path.isfile(inputvideo)):
     print("Can not open re_encoded video file " + reencode_video_name)
     exit(-1)
-    
+
 script_dir = dirname + ('/' if dirname else '') + 'autosub'
 script_fn = script_dir + '/' + basename + '.krfss'
 print("Script file: " + script_fn)
@@ -99,6 +100,7 @@ frame_cmds = {}
 frame_subindex = {}
 frame_haveblank = {}
 frame_havenmtg = {}
+frame_nmtgtransition = {}
 frame_index_temp = 0
 frame_index_temp1 = 0
 
@@ -109,7 +111,7 @@ with open(script_fn, mode='r', encoding='utf-8') as fp:
     if (text[0].encode('utf-8') == codecs.BOM_UTF8):
         text = text[1:]
     script = json.loads(text)
-    
+
 for command in script["timestamp"]:
     frame = int(command["at"])
     action = command["action"]
@@ -139,9 +141,12 @@ for command in script["timestamp"]:
         for i in range(frame_index_temp1, frame + 1):
             frame_havenmtg[i] = False
         frame_index_temp1 = frame
+    if (action == 'N'):
+        nmtg_y = int(command["y"])
+        frame_nmtgtransition[frame] = nmtg_y
 
-print (script["trans"])
-print (script["nmtgs"])
+print(script["trans"])
+print(script["nmtgs"])
 
 # POSITION DEFINITION
 ROI = (slice(500, 710), slice(70, 1140))
@@ -157,23 +162,33 @@ ROI_LINE0_Y = 84
 ROI_LINE1_Y = 136
 ROI_LINE0_Y_CENTER = ROI_LINE0_Y + LINE_HEIGHT / 2
 ROI_LINE1_Y_CENTER = ROI_LINE1_Y + LINE_HEIGHT / 2
-ROI_LINE0 = (slice(ROI_LINE0_Y, ROI_LINE0_Y + LINE_HEIGHT), slice(LINE_START, LINE_START + LINE_LENGTH))
-ROI_LINE1 = (slice(ROI_LINE1_Y, ROI_LINE1_Y + LINE_HEIGHT), slice(LINE_START, LINE_START + LINE_LENGTH))
+ROI_LINE0 = (slice(ROI_LINE0_Y, ROI_LINE0_Y + LINE_HEIGHT),
+             slice(LINE_START, LINE_START + LINE_LENGTH))
+ROI_LINE1 = (slice(ROI_LINE1_Y, ROI_LINE1_Y + LINE_HEIGHT),
+             slice(LINE_START, LINE_START + LINE_LENGTH))
+ROI_LINE0_Y_CENTER_3LINES = 72 + LINE_HEIGHT / 2
+ROI_LINE1_Y_CENTER_3LINES = 110 + LINE_HEIGHT / 2
+ROI_LINE2_Y_CENTER_3LINES = 148 + LINE_HEIGHT / 2
+ROI_LINE_Y_CENTER_3LINES_MAP = [
+    ROI_LINE0_Y_CENTER_3LINES, ROI_LINE1_Y_CENTER_3LINES, ROI_LINE2_Y_CENTER_3LINES]
 
 # COLOR
 TEXT_COLOR = (60, 66, 111)
 BOLD_COLOR = (0x8e, 0x66, 0xea)
-NMTG_COLOR = (255,255,255)
+NMTG_COLOR = (255, 255, 255)
 
 video = cv2.VideoCapture(reencode_video_name)
 fps = video.get(cv2.CAP_PROP_FPS)
 # Iphone's video is rotated
-width = max(video.get(cv2.CAP_PROP_FRAME_HEIGHT), video.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = min(video.get(cv2.CAP_PROP_FRAME_HEIGHT), video.get(cv2.CAP_PROP_FRAME_WIDTH))
+width = max(video.get(cv2.CAP_PROP_FRAME_HEIGHT),
+            video.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = min(video.get(cv2.CAP_PROP_FRAME_HEIGHT),
+             video.get(cv2.CAP_PROP_FRAME_WIDTH))
 # temp output without audio track
 out_name = inputvideo + '_temp_out.m4v'
 fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-out_video = cv2.VideoWriter(out_name, int(fourcc), fps, (int(width), int(height)))
+out_video = cv2.VideoWriter(out_name, int(
+    fourcc), fps, (int(width), int(height)))
 
 frame = 0
 font_text = ImageFont.truetype(fontf, int(options.fontsize))
@@ -181,8 +196,10 @@ font_nmtg = ImageFont.truetype(fontf, int(options.fontsize_nmtg))
 font_size_max = 1 * max(int(options.fontsize), int(options.fontsize_nmtg)) + 20
 img_draw_temp = Image.new("RGB", (font_size_max * 20, font_size_max))
 draw_temp = ImageDraw.Draw(img_draw_temp)
-font_w_text, font_h_text = draw_temp.textsize("LIPFgYTjyXSq|^#%", font = font_text)
-font_w_nmtg, font_h_nmtg = draw_temp.textsize("LIPFgYTjyXSq|^#%", font = font_nmtg)
+font_w_text, font_h_text = draw_temp.textsize(
+    "LIPFgYTjyXSq|^#%", font=font_text)
+font_w_nmtg, font_h_nmtg = draw_temp.textsize(
+    "LIPFgYTjyXSq|^#%", font=font_nmtg)
 img_nmtg_blank = cv2.imread(nmtg_blank_f)
 
 # static string
@@ -198,7 +215,7 @@ while(video.isOpened()):
         break
     time = frame / fps
     frame += 1
-    
+
     # Iphone's video is rotated
     if (video.get(cv2.CAP_PROP_FRAME_HEIGHT) > video.get(cv2.CAP_PROP_FRAME_WIDTH)):
         img_rot = np.rot90(img)
@@ -223,55 +240,68 @@ while(video.isOpened()):
             str_typed_cache = ''
             print(frame, "E")
             print(str_todraw)
-            
+
     is_blank = frame_haveblank[frame]
     is_nmtg = frame_havenmtg[frame]
 
     if (is_blank and not options.overlap):
         # binarization
         img_gray = cv2.cvtColor(img_crop, cv2.COLOR_BGR2GRAY)
-        retval, img_bin = cv2.threshold(img_gray, int(options.gray_threshold), 255, cv2.THRESH_BINARY)
+        retval, img_bin = cv2.threshold(img_gray, int(
+            options.gray_threshold), 255, cv2.THRESH_BINARY)
         img_mask = cv2.bitwise_not(img_bin)
         img_mask[:ROI_LINE0_Y, :] = 0
-        img_mask[ROI_LINE1_Y + LINE_HEIGHT:,:] = 0
+        img_mask[ROI_LINE1_Y + LINE_HEIGHT:, :] = 0
         img_mask[:, :LINE_START] = 0
         img_mask[:, LINE_START + LINE_LENGTH:] = 0
         neiborhood8 = np.array([[1, 1, 1],
-                        [1, 1, 1],
-                        [1, 1, 1]],
-                        np.uint8)
+                                [1, 1, 1],
+                                [1, 1, 1]],
+                               np.uint8)
         img_mask = cv2.dilate(img_mask, neiborhood8, iterations=2)
         # Inpaint to repair?
         img_inpaint = cv2.inpaint(img_crop, img_mask, 3, cv2.INPAINT_TELEA)
     else:
         img_inpaint = img_crop
-    
+
     nmtg = script["nmtgs"][script["nmtg_map"][index_sub]]
     if (is_nmtg and len(nmtg) and not options.overlap):
         img_inpaint[ROI_NMTG] = img_nmtg_blank
-        
+
+    if (frame in frame_nmtgtransition):
+        nmtg_y = frame_nmtgtransition[frame]
+        img_inpaint[(slice(ROI_NMTG_Y0 + nmtg_y, 50 + nmtg_y),
+                     slice(ROI_NMTG_X0, 395))] = img_nmtg_blank
+
     if (is_blank):
         img_pil = Image.fromarray(img_inpaint)
         draw = ImageDraw.Draw(img_pil)
-        str_typed_render = str_typed_cache[:min(len(str_typed_cache), int(float(options.typed_speed) * (frame - last_typed_start + 1)))]
+        str_typed_render = str_typed_cache[:min(len(str_typed_cache), int(
+            float(options.typed_speed) * (frame - last_typed_start + 1)))]
         str_splited = (str_todraw + str_typed_render).split('\n')
+        num_of_lines = len(str_splited)
         for (lineindex, text_line) in enumerate(str_splited):
             draw_x = LINE_START
-            draw_y0 = ROI_LINE1_Y_CENTER if (lineindex > 0) else ROI_LINE0_Y_CENTER
+            draw_y0 = ROI_LINE1_Y_CENTER if (
+                lineindex > 0) else ROI_LINE0_Y_CENTER
+            if (num_of_lines == 3):
+                draw_y0 = ROI_LINE_Y_CENTER_3LINES_MAP[lineindex]
             for (spanindex, text_span) in enumerate(text_line.split('$')):
                 color = TEXT_COLOR if (spanindex % 2 == 0) else BOLD_COLOR
-                span_w, span_h = draw.textsize(text_span, font = font_text)
+                span_w, span_h = draw.textsize(text_span, font=font_text)
                 draw_y = draw_y0 - font_h_text / 2
-                draw.text((draw_x, draw_y), text_span, fill = color, font = font_text)
+                draw.text((draw_x, draw_y), text_span,
+                          fill=color, font=font_text)
                 draw_x += span_w
-        
+
     if (is_nmtg and len(nmtg)):
         img_draw_nmtg = Image.new("RGB", img_nmtg_blank.shape[:2])
         draw_nmtg = ImageDraw.Draw(img_draw_nmtg)
-        w_nmtg, h_nmtg = draw.textsize(nmtg, font = font_nmtg)
+        w_nmtg, h_nmtg = draw.textsize(nmtg, font=font_nmtg)
         draw.text(
-            (ROI_NMTG_X0 + NM_NMTG_X_CENTER - w_nmtg / 2, ROI_NMTG_Y0 + NM_NMTG_Y_CENTER - font_h_nmtg / 2),
-            nmtg, fill = NMTG_COLOR, font = font_nmtg
+            (ROI_NMTG_X0 + NM_NMTG_X_CENTER - w_nmtg / 2,
+             ROI_NMTG_Y0 + NM_NMTG_Y_CENTER - font_h_nmtg / 2),
+            nmtg, fill=NMTG_COLOR, font=font_nmtg
         )
 
     if (is_blank):
@@ -285,9 +315,12 @@ while(video.isOpened()):
 video.release()
 out_video.release()
 
-ffcmd = "ffmpeg -y -vn -i " + reencode_video_name + " -acodec copy " + reencode_video_name + ".aac"
-print (ffcmd)
+ffcmd = "ffmpeg -y -vn -i " + reencode_video_name + \
+    " -acodec copy " + reencode_video_name + ".aac"
+print(ffcmd)
 subprocess.call(ffcmd, shell=True, env=env)
-ffcmd = "ffmpeg -y -i " + out_name + " -i " + reencode_video_name + ".aac -vcodec " + options.ffmpeg_encoder + " -preset slow -profile:v high -level:v 4.1 -pix_fmt yuv420p -b:v " + options.bitrate+ " -r 30 -acodec aac -strict -2 -ac 2 -ab " + options.audio_bitrate + " -ar 44100 -f flv " + inputvideo + ".autosubed.flv"
-print (ffcmd)
+ffcmd = "ffmpeg -y -i " + out_name + " -i " + reencode_video_name + ".aac -vcodec " + options.ffmpeg_encoder + " -preset slow -profile:v high -level:v 4.1 -pix_fmt yuv420p -b:v " + \
+    options.bitrate + " -r 30 -acodec aac -strict -2 -ac 2 -ab " + \
+        options.audio_bitrate + " -ar 44100 -f flv " + inputvideo + ".autosubed.flv"
+print(ffcmd)
 subprocess.call(ffcmd, shell=True, env=env)
