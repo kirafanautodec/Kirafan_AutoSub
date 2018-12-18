fd711e11ccce3ead4ad0b0a7a113c5dbeb3b629a
Ayaya_Komichi <kirafan_autodec@yahoo.co.jp>
2018-12-18 13:47:44 +0900

    change version to 10.0.0

--- analyse_video.py
+++ analyse_video.py
@@ -441,7 +441,7 @@ def analyse_video(args):
         json_trans = [''] * len(nmtg_map)
         json_data = {
             "video": basename,
-            "version": '8.1.2',
+            "version": '10.0.0',
             "total": len(nmtg_map),
             "lang": currentlang,
             "title": "",
